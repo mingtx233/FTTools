d2600f21573cad13a10b5c4772bd940650d19f6b
MingTX <yimingli233@outlook.com>
2026-05-10 12:15:52 +0800

    sync

---
+++ test_file_net_ops_client.py
@@ -0,0 +1,8 @@
+from Utils.FileNetOps import send_file_client
+
+server_ip     = "192.168.1.100"
+server_port   = 10023
+
+#send_file_client(server_ip, server_port, b"abcde", r".\send\vps.png")
+send_file_client(server_ip, server_port, b"abcde", r".\send")
+
---
+++ test_file_net_ops_server.py
@@ -0,0 +1,7 @@
+from Utils.FileNetOps import recv_file_server
+
+server_ip     = "192.168.1.100"
+server_port   = 10023
+
+recv_server = recv_file_server(server_ip, server_port, b"abcde", ".\\recv")
+
